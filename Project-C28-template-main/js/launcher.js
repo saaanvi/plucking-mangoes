c2262602449a318908f39d88b2c7feb980adda5f
saaanvi <76955989+saaanvi@users.noreply.github.com>
2021-05-27 10:22:53 +0530

    Add files via upload

---
+++ Project-C28-template-main/js/launcher.js
@@ -0,0 +1,36 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        
+        this.Launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+
+    fly(){
+        this.launcher.bodyA = null;
+    }
+
+    display(){
+
+        
+        
+
+        if(this.launcher.bodyA){
+            var pointA = this.launcher.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(7);
+           // line(pointA.x, pointA.y, pointB.x, pointB.y);
+           stroke(48,22,8);
+
+           
+                
+    }
+    }
+    
+}
